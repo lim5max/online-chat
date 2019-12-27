1f2fd98c8567f9f1fc3dcbeeb9e4835f6c72cfeb
lim5max <lionmaks2017@yandex.ru>
2019-12-27 21:52:20 +0300

    c

---
+++ app.js
@@ -0,0 +1,37 @@
+// Загрузка
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 4000;
+
+// Шаблоны
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Юзеры
+users = [];
+// Соединение
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log(data);
+      
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   
+   socket.on('msg', function(data) {
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+   })
+});
+
+server.listen(port, () => {
+    console.log('Server listening at port %d', port);
+  });
+  
